6a653e8cc209b8bbffcbf2678c9862a25d11f481
Diego Gonzalez <diego.gonzalez@docuten.com>
2021-02-16 13:44:22 +0100

    update

--- phej/js/Phej.js
+++ phej/js/Phej.js
@@ -121,15 +121,40 @@ function initializeGrid(){
 
 
     //Replace current character and move to the next  one
-    input.addEventListener('keypress', function(event){
-        var s = this.selectionStart;
-        $('#loopContent').html($('#loopContent').html().substr(0, s) + $('#loopContent').html().substr(s + 1));
-        this.selectionEnd = s;
+    input.onkeydown = function() {
+        var s = getCaretPosition(this);
+        $('#loopContent').html($('#loopContent').html().substr(0, s) + String.fromCharCode(event.keyCode) + $('#loopContent').html().substr(s + 1));
+        return false;
         
-    }, false);
+    }
 
 
 }
+
+function getCaretPosition(editableDiv) {
+    var caretPos = 0,
+      sel, range;
+    if (window.getSelection) {
+      sel = window.getSelection();
+      if (sel.rangeCount) {
+        range = sel.getRangeAt(0);
+        if (range.commonAncestorContainer.parentNode == editableDiv) {
+          caretPos = range.endOffset;
+        }
+      }
+    } else if (document.selection && document.selection.createRange) {
+      range = document.selection.createRange();
+      if (range.parentElement() == editableDiv) {
+        var tempEl = document.createElement("span");
+        editableDiv.insertBefore(tempEl, editableDiv.firstChild);
+        var tempRange = range.duplicate();
+        tempRange.moveToElementText(tempEl);
+        tempRange.setEndPoint("EndToEnd", range);
+        caretPos = tempRange.text.length;
+      }
+    }
+    return caretPos;
+  }
 
 var algo;
 
